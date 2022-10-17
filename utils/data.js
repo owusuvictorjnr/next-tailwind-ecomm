2f05f4f585a9c1122881b32845df36c30bcdd181
owusuvictorjnr <owusuvictor80@gmail.com>
2022-10-17 11:38:54 +0000

    Style card and primary-button

---
+++ utils/data.js
@@ -0,0 +1,96 @@
+const data = {
+  products: [
+    {
+      name: "Gents Set",
+      slug: "Gents Set",
+      category: "Gents ware",
+      image: "/images/gent1.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+
+    {
+      name: "Kids Set",
+      slug: "Kids Set",
+      category: "Kids ware",
+      image: "/images/kids1.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+
+    {
+      name: "kids Set",
+      slug: "kids Set",
+      category: "Kids ware",
+      image: "/images/kids2.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+
+    {
+      name: "Office Set",
+      slug: "Office Set",
+      category: "Office ware",
+      image: "/images/office2.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+
+    {
+      name: " Set1",
+      slug: "Set",
+      category: "Gents ware",
+      image: "/images/set1.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+
+    {
+      name: " Set2",
+      slug: "Gents Set",
+      category: "Gents ware",
+      image: "/images/set2.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+
+    {
+      name: "Tops",
+      slug: "Unisex",
+      category: "Unisex",
+      image: "/images/tops.jpg",
+      price: "100",
+      brand: "Vitech",
+      rating: "4.5",
+      numReviews: "8",
+      countInStock: "20",
+      description: "A popular shirt",
+    },
+  ],
+};
+
+export default data;
