d0dc6f9e224a91c2687691db0c4a13c0ad060ce7
owusuvictorjnr <owusuvictor80@gmail.com>
2022-10-17 11:10:32 +0000

    Render Products

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
-import Layout from '../components/Layout';
+import Layout from "../components/Layout";
+import data from "../utils/data";
+import ProductItem from "../components/ProductItem";
 
 export default function Home() {
   // i pass the title as a prop in the layout to get the
   // title on the tabbar
-  return <Layout title="Home Page">Home</Layout>;
+  return (
+    <Layout title="Home Page">
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+        {data.products.map((product) => (
+          <ProductItem product={product} key={product.slug}></ProductItem>
+        ))}
+      </div>
+    </Layout>
+  );
 }
