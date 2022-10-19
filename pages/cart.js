2ad3217df1fedd0dae642ce9b4cf16ba0b976a46
owusuvictorjnr <owusuvictor80@gmail.com>
2022-10-19 19:57:58 +0000

    add useRouter for removeItemHandler

--- pages/cart.js
+++ pages/cart.js
@@ -1,13 +1,21 @@
+import Image from "next/image";
 import Link from "next/link";
 import React, { useContext } from "react";
+import { XCircleIcon } from "@heroicons/react/outline";
 import Layout from "../components/Layout";
 import { Store } from "../utils/Store";
+import { useRouter } from "next/router";
 
 const CartScreen = () => {
+  const router = useRouter();
   const { state, dispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const removeItemHandler = (item) => {
+    dispatch({ type: "CART_REMOVE_ITEM", payload: item });
+  };
   return (
     <Layout title="Shopping Cart">
       <h1 className="mb-4 text-xl">Shopping Cart</h1>
@@ -16,10 +24,66 @@ const CartScreen = () => {
           Cart is empty. <Link href="/"> Go shopping</Link>
         </div>
       ) : (
-        <div className="grid md:grid-cols-4 md:gap-5"> 
-        <div className="overflow-x-auto md:col-span-3">
-            
-        </div>
+        <div className="grid md:grid-cols-4 md:gap-5">
+          <div className="overflow-x-auto md:col-span-3">
+            <table className="min-w-full">
+              <thead className="border-b">
+                <tr>
+                  <th className="px-5 text-left">Item</th>
+                  <th className="px-5 text-right">Quantity</th>
+                  <th className="px-5 text-right">Price</th>
+                  <th className="p-5">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {cartItems.map((item) => (
+                  <tr key={item.slug} className="border-b">
+                    <td>
+                      <Link href={`/product/${item.slug}`}>
+                        <a className="flex items-center">
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            width={50}
+                            height={50}
+                          />
+                          &nbsp;
+                          {item.name}
+                        </a>
+                      </Link>
+                    </td>
+                    <td className="p-5 text-right">{item.quantity}</td>
+                    <td className="p-5 text-right">&#8373; {item.price}</td>
+                    <td className="p-5 text-center">
+                      <button type="" onClick={() => removeItemHandler(item)}>
+                        <XCircleIcon className="h-5 w-5" />
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+          <div className="card p-5">
+            <ul>
+              <li>
+                <div className="pb-3 text-xl">
+                  Subtotal ({cartItems.reduce((a, c) => a + c.quantity, 0)}) :
+                  &#8373;
+                  {cartItems.reduce((a, c) => a + c.quantity * c.price, 0)}
+                </div>
+              </li>
+
+              <li>
+                <button
+                  onClick={() => router.push("/shipping")}
+                  className="primary-button w-full"
+                >
+                  Check Out
+                </button>
+              </li>
+            </ul>
+          </div>
         </div>
       )}
     </Layout>
