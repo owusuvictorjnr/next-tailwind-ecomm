1f16de1f3ce2309d2f3df688cfba2c65efce2649
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-26 22:09:16 +0000

    add message

--- pages/api/seed.js
+++ pages/api/seed.js
@@ -1,13 +1,14 @@
-import db from "../../db";
-import User from "../../models/User";
-import data from "../../utils/data";
+import db from '../../utils/db';
+import User from '../../models/User';
+import data from '../../utils/data';
 
 const handler = async (req, res) => {
   await db.connect();
   await User.deleteMany();
   await User.insertMany(data.users);
   await db.disconnect();
-  res.send({ message: "seeded successfully" });
+  res.send({ message: 'seeded successfully' });
 };
 
-export default handler
+export default handler;
+  
