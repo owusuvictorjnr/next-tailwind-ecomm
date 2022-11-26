a5afc5ebf64f64541f52252c345f126d575385bb
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-26 22:07:01 +0000

    .

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,7 @@ import { Store } from "../utils/Store";
 
 function Layout({ title, children }) {
   const { state, dispatch } = useContext(Store);
-  const { cart } = state;
+  const { cart } = state;  
   const [cartItemsCount, setCartItemsCount] = useState(0);
   useEffect(() => {
     setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
