3af024b3faa9ebb58692d7ee5f71e232e5bebb09
owusuvictorjnr <owusuvictor80@gmail.com>
2022-11-26 22:08:39 +0000

    comment out placeholders

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
-import Link from "next/link";
-import React from "react";
-import { useForm } from "react-hook-form";
-import Layout from "../components/Layout";
+import Link from 'next/link';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Layout from '../components/Layout';
 
 const Login = () => {
   const {
@@ -9,6 +9,7 @@ const Login = () => {
     register,
     formState: { errors },
   } = useForm();
+
   const submitHandler = ({ email, password }) => {
     console.log(email, password);
   };
@@ -19,52 +20,55 @@ const Login = () => {
       onSubmit={handleSubmit(submitHandler)}
     >
       <h1 className="mb-4 text-xl">Login</h1>
+
+      {/* Email */}
       <div className="mb-4">
         <label htmlfor="email">Email</label>
         <input
+          // name="email"
           type="email"
-          {...register("email", {
-            required: "Please enter your email",
+          {...register('email', {
+            required: 'Please enter your email',
             pattern: {
               value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
-              message: "Please enter a valid email",
+              message: 'Please enter a valid email',
             },
           })}
           id="email"
           className="w-full"
+          placeholder="Email"
           autoFocus
-          autoComplete="true"
         />
         {errors.email && (
           <div className="text-red-500">{errors.email.message}</div>
         )}
       </div>
 
+      {/* Password */}
       <div className="mb-4">
-        <label htmlfor="password">Password</label>
+        <label htmlfor="email">Password</label>
         <input
+          // name="Password"
           type="password"
-          {...register("password", {
-            required: "Please enter password",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters",
+          {...register('password', {
+            required: 'Please enter your password',
+            maxLength: {
+              values: 6,
+              message: 'Password is more than 6 characters ',
             },
           })}
           id="password"
           className="w-full"
+          placeholder="Password"
           autoFocus
-          autoComplete="true"
         />
         {errors.password && (
           <div className="text-red-500">{errors.password.message}</div>
         )}
       </div>
-
       <div className="mb-4">
         <button className="primary-button">Login</button>
       </div>
-
       <div className="mb-4">
         Don&apos;t have an account? &nbsp;
         <Link href="register">Register</Link>
